5b04a29fff67520e52bc10fc961440258eeff997
AngelLFMorante <angellfmorante@gmail.com>
2024-07-26 18:07:29 +0200

    initialization

---
+++ src/main/java/com/annunaki/gamebook/AnnunkiGameBookApplication.java
@@ -0,0 +1,13 @@
+package com.annunaki.gamebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnunkiGameBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnunkiGameBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annunaki/gamebook/AnnunkiGameBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annunaki.gamebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnunkiGameBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
